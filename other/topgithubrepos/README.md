89fbc1d21dca4603f827c8406ebfa87baaccd9cc
Paulo Ferreira <paulo.bsf@gmail.com>
2014-10-04 15:03:15 +0100

    Updated to recent version of the search API, refactored the code to support multiple languages and include stars count per language

--- other/topgithubrepos/README.md
+++ other/topgithubrepos/README.md
@@ -1,15 +1,5 @@
-Prints the top 10 Go repos on github (by number of stars)
+Prints the top 10 repos on github (by language & number of stars).
 
-Example output:
-```
-(5431)	go-martini/martini - Classy web framework for Go
-(4371)	astaxie/build-web-application-with-golang - A golang ebook intro how to build a web with golang
-(4253)	gogits/gogs - Gogs(Go Git Service) is a painless self-hosted Git Service written in Go.
-(4133)	revel/revel - A high productivity, full-stack web framework for the Go language.
-(3092)	astaxie/beego - beego is an open-source, high-performance web framework for the Go programming language.
-(2104)	hoisie/web - The easiest way to create web applications with Go
-(2034)	dropbox/godropbox - Common libraries for writing Go services/applications.
-(1933)	nsf/gocode - An autocompletion daemon for the Go programming language
-(1724)	codegangsta/cli - A small package for building command line apps in Go
-(1680)	buger/gor - HTTP traffic replay in real-time. Replay traffic from production to staging and dev environments.
-```
+Uses the github repo search API: https://developer.github.com/v3/search/#search-repositories
+
+See output file for results.
--- other/topgithubrepos/topgithubrepos.go
+++ other/topgithubrepos/topgithubrepos.go
@@ -7,52 +7,63 @@ import (
 	"net/http"
 )
 
+type Result struct {
+	Items []*Repository
+}
+
 type Repository struct {
-	Type         string
-	Username     string
-	Name         string
-	Owner        string
-	Homepage     string
-	Description  string
-	Language     string
-	Watchers     int
-	Followers    int
-	Forks        int
-	Size         int
-	OpenIssues   int
-	Score        float32
-	HasDownloads bool
-	HasIssues    bool
-	HasWiki      bool
-	Fork         bool
-	Private      bool
-	Url          string
-	Created      string
-	CreatedAt    string
-	PushedAt     string
-	Pushed       string
+	FullName      string `json:"full_name"`
+	Description   string
+	Homepage      string
+	WatchersCount int `json:"watchers_count"`
+	Language      string
 }
 
 func (r *Repository) String() string {
-	return fmt.Sprintf("(%d)\t%s/%s - %s\n", r.Followers, r.Owner, r.Name, r.Description)
+	return fmt.Sprintf("(%d)\t%s - %s", r.WatchersCount, r.FullName, r.Description)
 }
 
-func main() {
-	url := "https://api.github.com/legacy/repos/search/go?language=Go&sort=stars"
-	res, _ := http.Get(url)
+func GetTopRepos(language string, limit int) (string, int) {
+	var (
+		result     string
+		totalStars int
+		data       Result
+	)
 
+	url := fmt.Sprintf("https://api.github.com/search/repositories?q=+language:%s&sort=stars&order=desc", language)
+
+	res, _ := http.Get(url)
 	defer res.Body.Close()
 
 	body, _ := ioutil.ReadAll(res.Body)
 
-	var data map[string][]*Repository
-
 	json.Unmarshal(body, &data)
 
-	for i, v := range data["repositories"] {
-		fmt.Printf("%s", v)
-		if i == 9 {
-			break
+	for i, item := range data.Items {
+		result += fmt.Sprintf("%s\n", item)
+		totalStars += item.WatchersCount
+		if i == limit-1 {
+			break //	fugly... should I just use a regular for loop??
 		}
+	}
+
+	return result, totalStars
+}
+
+func main() {
+	languages := []string{
+		"go",
+		"java",
+		"scala",
+		"clojure",
+		"javascript",
+		"python",
+		"ruby",
+	}
+
+	for _, language := range languages {
+		repos, totalStars := GetTopRepos(language, 10)
+		fmt.Printf("%s (%d)\n--\n", language, totalStars)
+		fmt.Println(repos)
 	}
 }
